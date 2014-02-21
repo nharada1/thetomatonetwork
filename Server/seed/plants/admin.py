9af8ae7eb0f54468df47bc9c211032aad97b4545
Nate Harada <nharada1@gmail.com>
2014-02-20 19:30:51 -0500

    Removed Server folder in preparation for update

--- Server/seed/plants/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from plants.models import Arugula
-
-class PlantAdmin(admin.ModelAdmin):
-    list_display = ['id', 'initial_date', 'temperature', 'light_intensity', 'nutrient_density']
-    fields       = ['initial_date', 'temperature', 'light_intensity', 'nutrient_density']
-
-admin.site.register(Arugula, PlantAdmin)
