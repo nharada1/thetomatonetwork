c0b72618a49a18e8b9ab74e6ead360b9bab1e3a3
Sam Zeckendorf <sam.zeckendorf@gmail.com>
2014-03-05 13:11:25 -0500

    -n have proper parameters and tables for plants and states

--- plants/admin.py
+++ plants/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from plants.models import Arugula
+from plants.models import English_Ivy
+from plants.models import Plant_State
 
 class PlantAdmin(admin.ModelAdmin):
-    list_display = ['id', 'initial_date', 'temperature', 'light_intensity', 'nutrient_density']
-    fields       = ['initial_date', 'temperature', 'light_intensity', 'nutrient_density']
+    list_display = ['id', 'initial_date', 'user_name', 'plant_name']
+    fields       = ['initial_date', 'user_name', 'plant_name']
 
-admin.site.register(Arugula, PlantAdmin)
+class PlantStateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'timestep', 'date', 'nutrient_value', 'light_value', 'health_rating', 'plant']
+    fields       = ['timestep', 'date', 'nutrient_value', 'light_value', 'health_rating', 'plant']
+
+admin.site.register(English_Ivy, PlantAdmin)
+admin.site.register(Plant_State, PlantStateAdmin)
