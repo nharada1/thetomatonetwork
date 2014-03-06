e0dea4742420f9a9bad4a0f022c8d114aa667399
Sam Zeckendorf <sam.zeckendorf@gmail.com>
2014-03-06 00:12:23 -0500

    added algo metadata to the admin console

--- Server/seed/plants/admin.py
+++ Server/seed/plants/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from plants.models import English_Ivy
 from plants.models import Plant_State
+from plants.models import Algo_Metadata
 
 class PlantAdmin(admin.ModelAdmin):
     list_display = ['id', 'initial_date', 'user_name', 'plant_name']
@@ -10,5 +11,10 @@ class PlantStateAdmin(admin.ModelAdmin):
     list_display = ['id', 'timestep', 'date', 'nutrient_value', 'light_value', 'health_rating', 'plant']
     fields       = ['timestep', 'date', 'nutrient_value', 'light_value', 'health_rating', 'plant']
 
+class AlgoMetaDetaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'L', 'T']
+    fields =     = ['L', 'T']
+
 admin.site.register(English_Ivy, PlantAdmin)
 admin.site.register(Plant_State, PlantStateAdmin)
+admin.site.register(Algo_Metadata, AlgoMetaDetaAdmin)
