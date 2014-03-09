b37c2f54aef11486152a4f732ae38ef808a08ad9
Sam Zeckendorf <sam.zeckendorf@gmail.com>
2014-03-09 17:23:39 -0400

    changed to allow for default server settings

--- Server/seed/seed/settings.py
+++ Server/seed/seed/settings.py
@@ -159,9 +159,7 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-
-DATABASE_URL='postgres://localhost/seed_db'
-DATABASES['default'] =  dj_database_url.config(default=DATABASE_URL)
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
