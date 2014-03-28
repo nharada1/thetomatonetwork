ac61cde1982a828874679d3eb981c08a05d1d34d
jpothi01 <john.pothier7@gmail.com>
2014-03-27 21:32:27 -0400

    Added clipping to nutrient update algo to ensure valid duty cycles

--- LeafScan/avglambda.m
+++ LeafScan/avglambda.m
@@ -8,8 +8,8 @@ lambda_avg = 0;
 iter = 0;
 for i=1:size(img,1)
     for j=1:size(img,2)
-        % Ignore white pixels
-        if img(i,j,1) ~= 255 && img(i,j,2) ~= 255 && img(i,j,3) ~= 255
+        % Ignore black pixels
+        if img(i,j,1) ~= 0 && img(i,j,2) ~= 0 && img(i,j,3) ~= 0
             [x,y] = RGB2xy(squeeze(img(i,j,:)));
             lambda = xy2lambda(x,y,CIE);
             lambda_avg = (iter*lambda_avg+lambda)/(iter+1);
