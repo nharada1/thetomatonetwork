e8eac83605fd9d2864ed1153a65394e575bcbdb0
John Pothier <john.pothier7@gmail.com>
2014-03-29 22:20:47 -0400

    Adjusted learning rate and added plant photos

--- LeafScan/run.m
+++ LeafScan/run.m
@@ -1,7 +1,7 @@
-lambdas = zeros(5);
+lambdas = zeros(5,1);
 
 for i=0:4
-    path = ['img/cropped/' int2str(t) '/plant' int2str(i) '.jpg'];
+    path = ['img/raw/' int2str(t) '/plant' int2str(i) '.jpg'];
     img = imresize(imread(path),0.25);
     midx = floor(size(img,2)/2);
     midy = floor(size(img,1)/2);
@@ -14,8 +14,9 @@ for i=0:4
     for j=1:3
         img_cropped(:,:,j) = img(:,:,j).*uint8(mask);
     end
+    iamge(img_cropped)
     img_double = double(img_cropped);
-    inv_lambdas(1) = 1/avglambda(img_double);
+    lambdas(i) = avglambda(img_double);
 end
 %{
  path = ['img/cropped/' int2str(t) '/plant' int2str(1) '.jpg'];
