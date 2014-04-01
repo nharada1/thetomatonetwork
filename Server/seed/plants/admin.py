c3ba249e0a3d21fe4e008107f8edbc19151aa534
Sam Zeckendorf <sam.zeckendorf@gmail.com>
2014-03-31 20:59:21 -0400

    removed references to controlplantstates

--- Server/seed/plants/admin.py
+++ Server/seed/plants/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from plants.models import EnglishIvy
 from plants.models import PlantState
-from plants.models import ControlPlantState
 from plants.models import AlgoMetadata
 from plants.models import CareConstants
 
@@ -14,9 +13,6 @@ class PlantStateAdmin(admin.ModelAdmin):
     list_display = ['id', 'timestep', 'date', 'nutrient_value', 'light_value', 'performance_value', 'plant']
     fields       = ['timestep', 'date', 'nutrient_value', 'light_value', 'performance_value', 'plant']
 
-class ControlPlantStateAdmin(admin.ModelAdmin):
-    list_display = ['id', 'timestep', 'date', 'performance_value', 'plant']
-    fields       = ['timestep', 'date', 'performance_value', 'plant']
 
 
 class AlgoMetaDataAdmin(admin.ModelAdmin):
@@ -29,6 +25,5 @@ class CareConstantsAdmin(admin.ModelAdmin):
 
 admin.site.register(EnglishIvy, PlantAdmin)
 admin.site.register(PlantState, PlantStateAdmin)
-admin.site.register(ControlPlantState, ControlPlantStateAdmin)
 admin.site.register(AlgoMetadata, AlgoMetaDataAdmin)
 admin.site.register(CareConstants, CareConstantsAdmin)
